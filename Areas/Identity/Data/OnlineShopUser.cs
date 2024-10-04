8e2154d7e274038fcb27e177d043fa8db4e9c12e
Atikul1213 <atikul.islam@brainstation-23.com>
2024-10-05 00:05:29 +0600

    Add two property

--- Areas/Identity/Data/OnlineShopUser.cs
+++ Areas/Identity/Data/OnlineShopUser.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace OnlineShop.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the OnlineShopUser class
 public class OnlineShopUser : IdentityUser
 {
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
 }
 
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OnlineShop.Areas.Identity.Data;
 using OnlineShop.Models;
 
@@ -20,7 +21,21 @@ namespace OnlineShop.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
         }
 
+
+
+    }
+}
+
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<OnlineShopUser>
+{
+    public void Configure(EntityTypeBuilder<OnlineShopUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
     }
 }
